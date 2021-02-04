528ae4d3582758186386a05fc9a1dc742e05d847
Mnvss vamsi <vamsi.m07@gmail.com>
2021-02-04 22:19:45 +0530

    prac1 styles from prac2

--- src/components/prac1.js
+++ src/components/prac1.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const prac1 = () => {
     return (
-        <div>
+        <div className="prac1-styles-from2">
             originalprac file
            <p className="prac1">changing first time</p> 
            <p>changing prac1 second time</p>
