f49dbc41d68d4308a0043f077d0a71534c1cc36d
Mnvss vamsi <vamsi.m07@gmail.com>
2021-02-04 21:47:29 +0530

    changed mydetails to  prac2

--- src/components/prac2.js
+++ src/components/prac2.js
@@ -3,7 +3,8 @@ import React from 'react'
 const mydetails = () => {
     return (
         <div>
-            vamsi here
+            vamsi here 
+            with prac2 file
         </div>
     )
 }
